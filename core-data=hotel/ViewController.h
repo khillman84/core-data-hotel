49587c0bc8d37c4ab67abada78306595b0cf0513
Kyle Hillman <khillman84@gmail.com>
2017-04-24 09:28:36 -0700

    Initial Commit

---
+++ core-data=hotel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  core-data=hotel
+//
+//  Created by Kyle Hillman on 4/24/17.
+//  Copyright © 2017 Kyle Hillman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ core-data=hotel/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  core-data=hotel
+//
+//  Created by Kyle Hillman on 4/24/17.
+//  Copyright © 2017 Kyle Hillman. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
